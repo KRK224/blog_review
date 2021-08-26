afd573d7cfb90c7da304b063f98ed29048e31d6f
krk224 <hihas93@naver.com>
2021-08-26 11:37:41 +0900

    Rest API 구현 완료

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -143,6 +143,29 @@ export const read = async ctx => {
   }
 };
 
-export const remove = async ctx => {};
-export const update = async ctx => {};
+export const remove = async ctx => {
+  const {id} = ctx.params;
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204; // No Content
+  } catch (e) {
+    ctx.throw(500,e);
+  }
+};
+
+export const update = async ctx => {
+  const {id} = ctx.params;
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
+    }).exec();
+    if(!post){
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
